7d6395bbf1b42602df02a0135e900883b29c7db9
winifidelis <winifidelis@gmail.com>
2020-04-29 16:51:07 -0300

    no message

---
+++ exercicios-react/src/index.js
@@ -0,0 +1,8 @@
+//a linha abaixo é para colocar codigos 'JFX' dentro do javascript
+import React from 'react'
+
+import ReactDOM from 'react-dom'
+
+const elemento = document.getElementById('root')
+
+ReactDOM.render(<h2>teste</h2>, elemento)
