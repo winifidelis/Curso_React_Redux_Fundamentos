ce5033db4c52244aff83dc711c4a2adc7cafdb7e
Winicius <winicius.farias@aparecida.go.gov.br>
2020-05-13 13:29:36 -0300

    finalizado o backend, hora de iniciar o frontend

---
+++ mymoney/backend/src/config/cors.js
@@ -0,0 +1,11 @@
+//o CORS é uma proteção para solicitações feita por aplicações em outros endereços.
+//Mesmo que a aplicação esteja no mesmo IP, e ela estando em porta diferente
+//já configura um local diferente
+module.exports = (req, res, next) => {
+    //abaixo tenho cabeçalhos para habilitar o CORS
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    //como isso aqui é um middleware é necessário o next para dar continuidade a atividade
+    next()
+}
