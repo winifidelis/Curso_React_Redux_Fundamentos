c675a1af818ead213a6eed5a8f5a25d932de99a3
Winicius <winicius.farias@aparecida.go.gov.br>
2020-06-10 08:51:18 -0300

    no message

--- authMyMoney/backend/src/loader.js
+++ authMyMoney/backend/src/loader.js
@@ -1,3 +1,25 @@
-const server = require('./config/server')
+//------const server = require('./config/server')
+
+const port = 3003
+//fara o parse no corpo da requisição entregando um objeto pronto
+const bodyParser = require('body-parser')
+//require do node
+const express = require('express')
+const server = express()
+const allowCors = require('./config/cors')
+const queryParser = require('express-query-int')
+
+//toda requisição que chegar user o bodyparser quando o formato for urlencoded
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser())
+
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+//---module.exports = server
+
 require('./config/database')
 require('./config/routes')(server)
